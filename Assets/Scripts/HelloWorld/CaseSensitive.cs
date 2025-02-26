a1e0f5187bf3458957a6a46e52d167902beffc9b
DESKTOP-5V3FRMH\M <heoseokhyeon@gmail.com>
2025-02-26 17:20:49 +0900

    HelloWorld 구현

---
+++ Assets/Scripts/HelloWorld/CaseSensitive.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class CaseSensitive : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("C#은 대소문자를 구분합니다.");
+    }
+}
